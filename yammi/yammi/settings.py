fe25c20132ae2e39f4d124831020bb0c62179247
sangwonchoi <rngus3698@naver.com>
2020-10-21 00:09:08 +0900

    20201017_야미거래 update, pagination_sangwon

--- yammi/yammi/settings.py
+++ yammi/yammi/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
 
 ##
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
     'DEFAULT_PERMISSION_CLASSES': (
         # 'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.AllowAny',
--- yammi/yammiDeal/views.py
+++ yammi/yammiDeal/views.py
@@ -4,11 +4,27 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.exceptions import NotFound as NotFoundError
+from rest_framework import status
 
 from .models import Deal
+from rest_framework.pagination import PageNumberPagination
 
 
 # Create your views here.
+
+
+class MyPageNumberPagination(PageNumberPagination):
+   page_size = 10
+
+   def generate_response(self, query_set, serializer_obj, request):
+       try:
+           page_data = self.paginate_queryset(query_set, request)
+       except NotFoundError:
+           return Response({"error": "No results found for the requested page"}, status=status.HTTP_400_BAD_REQUEST)
+
+       serialized_page = serializer_obj(page_data, many=True)
+       return self.get_paginated_response(serialized_page.data)
 
 
 class DealView(APIView):
@@ -17,55 +33,46 @@ class DealView(APIView):
     authentication_classes = [JSONWebTokenAuthentication]
 
     def get(self, request):
-        my_deal = Deal.objects.all() #Profile객체의 전체를 뽑아온다
-        serializer = DealSerializer(my_deal, many=True)  #뽑아온 Profile 객체를 Json 타입으로 변환
+        queryset = Deal.objects.all() #Profile객체의 전체를 뽑아온다
+        serializer = DealSerializer(queryset, many=True)  #뽑아온 Profile 객체를 Json 타입으로 변환
         return Response(serializer.data)
 
     def post(self, request):
 
         my_profile = Profile.objects.get(user_id=request.data["user_id"])
 
+        print(int(request.data["deal_money"]))
+        #주는사람 profile update
+        my_profile.money = my_profile.money - int(request.data["deal_money"])
+        if(my_profile.money < 0):   #주는 금액이 가지고있는 금액을 초과하면 return
+            return Response({"거래": "실패"})
+        else:
+            my_profile.save()
+        #받는사람
+        take_profile = Profile.objects.get(user_id=request.data["user_profile_take"])
+        take_profile.money = take_profile.money + int(request.data["deal_money"])
+        take_profile.save()
+
+        #Deal 테이블 insert
         deal_obj = Deal.objects.create(user_take=request.data["user_profile_take"],
                                        deal_money=request.data["deal_money"], user_give=my_profile)
 
         deal_obj.save()
 
-        print(request.POST)
-
-        # give_serializer = ProfileSerializer(my_profile)
-        # # money = give_serializer.data["money"]-request.data["deal_money"]
-        # give_user = Profile.objects.update(#id=give_serializer.data["id"],
-        #                                    money=1500,
-        #                                    user_id=request.data["user_id"])
-        #
-        # take_profile = Profile.objects.get(user_id=request.dta["user_profile_take"])
-        # take_serializer = ProfileSerializer(take_profile)
-        # # money = take_serializer.data["money"]+request.data["deal_money"]
-        # take_user = Profile.objects.update(#id=request.data["user_profile_take"],
-        #                                    money=200,
-        #                                    user_id=take_serializer.data["user_id"])
-        # give_user.save()
-        # take_user.save()
         return Response({"거래": "성공"})
-
-    # def put(self, request):
-    #     my_profile = Profile.objects.get(user_id=request.data["user_id"])
-    #     serializer = ProfileSerializer(my_profile)
-    #     my_profile_ch = Profile.objects.update(user_id=serializer.data["user_id"], money=serializer.data["money"]+request.data["money"])
-    #     my_profile_ch.save()
-    #
-    #     return Response()
 
 
 class DetailDeal(APIView):
     permission_classes = [IsAuthenticated]
     # JWT 인증을 확인하기 위해 사용한다
     authentication_classes = [JSONWebTokenAuthentication]
+    pagination_class = PageNumberPagination
 
     def post(self, request):    #해당 유저의 송금, 입금 리스트 뽑
         user_id = request.data["user_id"]
-        my_give_deal = Deal.objects.filter(user_give_id=user_id) | Deal.objects.filter(user_take=user_id)
-        serializer = DealSerializer(my_give_deal, many=True)
+        queryset = Deal.objects.filter(user_give_id=user_id) | Deal.objects.filter(user_take=user_id)
+        paginator = MyPageNumberPagination()
+        response = paginator.generate_response(queryset, DealSerializer, request)
 
-        return Response(serializer.data)
+        return response
 
